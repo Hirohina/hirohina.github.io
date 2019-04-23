fdfe31f8aed380f96a61303cd73859fa0a68a1f2
Hirohina <archer.benjamin@ymail.com>
2019-04-23 11:55:49 +0200

    Update skillbar.js

--- scripts/skillbar.js
+++ scripts/skillbar.js
@@ -1,16 +1,21 @@
+var temp = 0; 
+
 $(window).scroll(function() {
-	var hT = $('#parallaxcomp').offset().top,
-		hH = $('#parallaxcomp').outerHeight(),
-		wH = $(window).height(),
-		wS = $(this).scrollTop();
-	 console.log((hT-wH) , wS);
-	if (wS > (hT+hH-wH)){
-	 jQuery(document).ready(function(){
-	 jQuery('.skillbar').each(function(){
-		 jQuery(this).find('.skillbar-bar').animate({
-			 width:jQuery(this).attr('data-percent')
-		 },3000);
+	if (temp == 0) {
+		var hT = $('#parallaxcomp').offset().top,
+			hH = $('#parallaxcomp').outerHeight(),
+			wH = $(window).height(),
+			wS = $(this).scrollTop();
+		 console.log((hT-wH) , wS);
+		if (wS > (hT+hH-wH)){
+		 jQuery(document).ready(function(){
+		 jQuery('.skillbar').each(function(){
+			 jQuery(this).find('.skillbar-bar').animate({
+				 width:jQuery(this).attr('data-percent')
+			 },3000);
+			  temp = 1;
+		 });
 	 });
- });
-	}
- });
+		}
+	 });
+}
